0961ef02c493951156b5096f6f5fed1f2826f15e
maheshkumartatikonda <41547441+maheshkumartatikonda@users.noreply.github.com>
2018-08-09 09:46:14 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 var express = require('express')
 var app = express()
+var bodyparser= require('body-parser')
+
 
 app.use(express.static(__dirname))
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended: false}))
 
 var messages = [
     {name: 'Tim', message: 'Hi'},
-    {name: 'Jane', message: 'Hello'}
+    {name: 'Jane', message: 'Hello'} 
 ]
 
 app.get('/messages', (req, res) =>{
     res.send(messages)
 })
 
-var server = app.listen(3000, () => {
-    console.log('server is listening on port', server.address().port)
+app.post('/messages', (req, res) =>{
+    messages.push(req.body)
+    console.log(req.body);
+    res.sendStatus(200)
+})
+
+
+var server = app.listen(3000, "mahesh-surface" , () => {
+    console.log('server is listening on host and  port and web dir:-', server.address().address, server.address().port, __dirname)
 })
