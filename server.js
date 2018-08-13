eae11f6bcd681c73d00c48cda41d48a06db0c544
maheshkumartatikonda <41547441+maheshkumartatikonda@users.noreply.github.com>
2018-08-13 08:47:41 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,28 +1,71 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
-var bodyparser= require('body-parser')
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
+mongoose.Promise = Promise 
 
 app.use(express.static(__dirname))
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
-var messages = [
-    {name: 'Tim', message: 'Hi'},
-    {name: 'Jane', message: 'Hello'} 
-]
+var dbUrl = 'mongodb://localhost:27017/'
 
-app.get('/messages', (req, res) =>{
-    res.send(messages)
+var Message = mongoose.model('messages', {
+    name: String,
+    message: String
 })
 
-app.post('/messages', (req, res) =>{
-    messages.push(req.body)
-    console.log(req.body);
-    res.sendStatus(200)
+app.get('/messages', (req, res) => {
+    Message.find({}, (err, messages) => {
+        res.send(messages)
+    })
+})
+
+app.get('/messages/:user', (req, res) => {
+    var user = req.params.user
+    Message.find({name: user}, (err, messages) => {
+        res.send(messages)
+    })
+})
+
+app.post('/messages', async (req, res) => {
+
+    try {
+        var message = new Message(req.body)
+
+        var savedMessage = await message.save()
+
+        console.log('saved')
+
+        var censored = await Message.findOne({ message: 'badword' })
+
+        if (censored)
+            await Message.remove({ _id: censored.id })
+        else
+            io.emit('message', req.body)
+
+        res.sendStatus(200)
+    } catch (error) {
+        res.sendStatus(500)
+        return console.error(error)
+    } finally {
+        console.log('message post called')
+    }
 })
 
 
-var server = app.listen(3000, "mahesh-surface" , () => {
-    console.log('server is listening on host and  port and web dir:-', server.address().address, server.address().port, __dirname)
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+})
+
+mongoose.connect(dbUrl, { useMongoClient: true }, (err) => {
+    console.log('mongo db connection', err)
+})
+
+var server = http.listen(3000, () => {
+    console.log('server is listening on port', server.address().port)
 })
